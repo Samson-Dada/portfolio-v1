432e86f2f5602c437004388c35ab33c6a709c2f1
Samson Dada <57550434+Samson-Dada@users.noreply.github.com>
2024-07-31 10:03:43 +0100

    Feat: Add links, web pages to projects

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const menuToggle = document.getElementById("menu-toggle");
 	const navMenu = document.getElementById("nav-menu");
-	const menuIcon = document.getElementById("menu-icon");
+	const menuIcon = document.querySelector(".menu-icon use");
 	const copyRight = document.querySelector("#copyright_text");
 
-	copyRight.textContent = `© ${new Date().getFullYear()} Samson Dada. All rights reserved.`;
+	copyRight.textContent = `© ${new Date().getFullYear()} All Right Reserved | Built by Samson Dada | 👋🏽`;
 
 	let menuOpen = false;
-
 	const toggleMenu = () => {
 		menuOpen = !menuOpen;
 		navMenu.classList.toggle("-translate-x-full", !menuOpen);
-		menuIcon.textContent = menuOpen ? "close" : "menu"; // Change between 'close' and 'menu' icons
+		menuIcon.setAttribute(
+			"xlink:href",
+			menuOpen
+				? "icons/Material-icon.svg#icon-cancel"
+				: "icons/Material-icon.svg#icon-menu"
+		);
 	};
 
 	menuToggle.addEventListener("click", toggleMenu);
+
+	const navLinks = document.querySelectorAll(".nav__link");
+
+	navLinks.forEach((link, index) => {
+		link.addEventListener("click", function (event) {
+			event.preventDefault();
+			const targetId = this.getAttribute("href").substring(1);
+			console.log("targetId", targetId);
+			const targetSection = document.getElementById(targetId);
+			console.log("targetSection", targetSection);
+
+			if (menuOpen) {
+				toggleMenu();
+			}
+
+			window.scrollTo({
+				top: targetSection.offsetTop,
+				behavior: "smooth",
+			});
+		});
+	});
 });
